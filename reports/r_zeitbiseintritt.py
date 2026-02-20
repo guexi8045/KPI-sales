4485db538145f682e62a535387f93213268c526c
guexi8045 <support@guexi.ch>
2026-02-20 14:19:51 +0100

    Keine Durchschnitstage mehr

--- reports/r_zeitbiseintritt.py
+++ reports/r_zeitbiseintritt.py
@@ -5,17 +5,17 @@ def get_zeitbiseintritt():
     query = """
         SELECT 
             id,
-            abschlussdatum,
-            eintrittsdatum,
+            abschlussdatum::date AS abschlussdatum,
+            eintrittsdatum::date AS eintrittsdatum,
             TO_CHAR(abschlussdatum::date, 'YYYY-MM') AS month,
-            EXTRACT(DAY FROM eintrittsdatum::timestamp - abschlussdatum::timestamp) AS anzahl_tage
+            (eintrittsdatum::date - abschlussdatum::date) AS anzahl_tage
         FROM deals
-        WHERE abschlussdatum IS NOT NULL AND eintrittsdatum IS NOT NULL
+        WHERE abschlussdatum IS NOT NULL
+          AND eintrittsdatum IS NOT NULL
+        ORDER BY month ASC, abschlussdatum ASC, eintrittsdatum ASC, id ASC
     """
     conn = get_connection()
     df = pd.read_sql_query(query, conn)
     conn.close()
-    df_grouped = df.groupby('month')['anzahl_tage'].mean().reset_index()
-    df_grouped['category_name'] = 'Ø Tage bis Eintritt'
-    df_grouped['num_deals'] = df_grouped['anzahl_tage'].round(2)
-    return df_grouped[['month', 'category_name', 'num_deals']].to_dict(orient="records")
+
+    return df.to_dict(orient="records")
